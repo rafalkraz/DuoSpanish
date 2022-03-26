7f943799b7a9902e81533ad2387bfdc2801f2310
rafal-0101 <rafalkraz@gmail.com>
2022-03-26 20:45:50 +0100

    electron packager installation

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const { app, BrowserWindow } = electron;
 app.on('ready', () => {
     const mainWindow = new BrowserWindow;
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.maximize();
 });
